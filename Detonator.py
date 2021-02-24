4a8ae83bb618ed5943cbfb38074d47baf9736064
Tino <67396655+tinocoppens@users.noreply.github.com>
2021-02-24 22:00:55 +0100

    Add files via upload

---
+++ Detonator.py
@@ -0,0 +1,16 @@
+import automationhat
+detonatetrueorfalse = input('Detonate ? (y/n) : ')
+if detonatetrueorfalse == "y":
+    print("3")
+    time.sleep(1.0)
+    print("2")
+    time.sleep(1.0)
+    print("1")
+    time.sleep(1.0)
+    print("Detonate")
+    automationhat.relay.one.toggle()
+if detonatetrueorfalse == "n":
+    print('Ok')
+else:
+    print('Invalid')
+#with https://shop.pimoroni.com/products/automation-hat
